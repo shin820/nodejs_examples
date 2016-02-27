4c9ea888bc32fbde8151dfc1153f0a060135cee5
shin820@163.com <shin820@163.com>
2016-02-27 14:45:57 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Shin.Liu on 2/27/2016.
+ */
+var http=require("http");
+http.createServer(function(request,response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8888);
+console.log('Server running at http://127.0.0.1:8888/');
